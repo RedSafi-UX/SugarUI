75502ee2c8b992878dd9399a84c9b712d59e1fd9
redsafi@live.com <redsafi@live.com>
2013-01-29 15:36:13 +0800

    N

---
+++ SurgerUI.Sweet/SurgerUI.Sweet/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SurgerUI.Sweet
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MyProgressbar.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            MyProgressbar2.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            //MyProgressRing.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            this.Loaded += MainPage_Loaded;
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var xx = Application.Current.RequestedTheme;
+            var brush = Application.Current.Resources["CheckBoxBackgroundThemeBrush"] as SolidColorBrush;
+            var color = brush.Color;
+            var xxxx = 1;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        
+    }
+}
